d39f9d7232a02a3b71dccfecddafdb92604ca8f6
Godwin Opara <godwinopara62@yahoo.com>
2022-09-21 16:10:41 +0100

    inserted data into database

--- app.js
+++ app.js
@@ -6,10 +6,24 @@ const db = new sqlite3.Database("practice.db", sqlite3.OPEN_READWRITE, (err) => 
     console.log("connected to database successfully")
 })
 
-const table = `CREATE TABLE friends(
+const table = `CREATE TABLE IF NOT EXISTS friends(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL
 )`;
 
 db.run(table)
+
+const friend = `INSERT INTO friends(first_name, last_name)
+VALUES
+    ('Jehovah', 'Shammah'),
+    ('Adekunle', 'Sewanu'),
+    ('Opara', 'Kelechi'),
+    ('Opara', 'Tochukwu'),
+    ('Opara', 'Onukwugha');
+`
+db.run(friend)
+
+db.run(`INSERT INTO friends (first_name, last_name)
+VALUES ('Jenny', 'Who');`)
+
